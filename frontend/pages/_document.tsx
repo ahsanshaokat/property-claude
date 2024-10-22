01b78e752be34956cfb8347d90727afa393fb8b3
AhsanShaokatNBS <ahsan.shaokat@northbaysolutions.net>
2024-10-23 00:35:55 +0500

    df

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-title-in-document-head */
+/* eslint-disable */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,7 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="Ekzameen.com is your go-to platform for buying, selling, or renting real estate properties. Discover your dream home or perfect investment today!" />
-        
+        <meta property="og:title" content="ekZameen.com - Buy, Sell or Rent" />
+        <meta property="og:site_name" content="ekZameen.com" />
+        <meta property="og:url" content="https://ekzameen.com" />
+        <meta property="og:description" content="Ekzameen.com is your go-to platform for buying, selling, or renting real estate properties. Discover your dream home or perfect investment today!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://ekzameen.com/images/1024.png" />
       </Head>
       <body>
         <Main />
