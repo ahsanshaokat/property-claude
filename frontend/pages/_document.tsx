27c4aed6985dd26dbc846a625bbb03b63b66082a
AhsanShaokatNBS <ahsan.shaokat@northbaysolutions.net>
2024-10-23 16:55:58 +0500

    df

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Meta tags */}
         <meta name="description" content="Ekzameen.com is your go-to platform for buying, selling, or renting real estate properties. Discover your dream home or perfect investment today!" />
         <meta property="og:title" content="ekZameen.com - Buy, Sell or Rent" />
         <meta property="og:site_name" content="ekZameen.com" />
@@ -12,6 +13,22 @@ export default function Document() {
         <meta property="og:description" content="Ekzameen.com is your go-to platform for buying, selling, or renting real estate properties. Discover your dream home or perfect investment today!" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://ekzameen.com/images/1024.png" />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-K47389KZ5X"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K47389KZ5X');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
